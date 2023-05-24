72f8bc81b853f7106715451d690cc93128f76695
Agustin Luparini <109320574+ellupa@users.noreply.github.com>
2023-05-24 20:49:11 -0300

    Creacion de las clases Materias y otras
    Creacion de la clase materias, materias por plan, detalle estudiante, Mesa de examen y insciptos por mesa

---
+++ IncripcionAFianelesBackend/Modelos/DetalleEstudiantes.php
@@ -0,0 +1,18 @@
+<?php 
+ public class DetalleEstudiante
+ {
+    public int $ID;
+    public Estudiante $Estudiante;
+    public Materia $Materia;
+    public string $Estado ;
+
+    public function _construct(int $id, Estudiante $estudiante,Materia $materias ,string $estado)
+    {
+        $this->$ID =$id;
+        $this->$Estudiante = $estudiantE;
+        $this->$Materia = $materias;
+        $this->$Estado = $estado;
+    }
+
+ }
+?>
---
+++ IncripcionAFianelesBackend/Modelos/IncriptosPorMesa.php
@@ -0,0 +1,17 @@
+<?php 
+public class IncriptoPorMesa
+{
+    public int $ID;
+    public MesaDeExamen $Mesa;
+    public Estudiante $Estudiante ;
+
+    public function _construct(int $id, MesaDeExamen $mesa,Estudiante $estudiante)
+    {
+        $this->$ID = $id;
+        $this->$Mesa = $Mesa;
+        $this->$Estudiante = $estudiante;
+
+    }
+}
+
+?>
